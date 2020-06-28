2bc67824691fc49067e8f3277f6ffcc32cb3e198
Jeremías <jrmsdev@gmail.com>
2020-06-27 22:50:19 -0300

    docker do not force --pull but accept cmdline args

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
-docker build --pull --rm -t jrmsdev/munbot:base \
+docker build $@ --rm -t jrmsdev/munbot:base \
 	--build-arg MUNBOT_UID=$(id -u) \
 	--build-arg MUNBOT_GID=$(id -g) \
 	./docker/base
