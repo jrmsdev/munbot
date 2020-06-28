2f70f0661770d8f0eba60f89f0b530dfda39999e
Jeremías <jrmsdev@gmail.com>
2020-06-27 21:21:30 -0300

    docker publish api http port

--- docker/devel/run.sh
+++ docker/devel/run.sh
@@ -2,6 +2,8 @@
 set -eu
 
 docker run -it --rm --name munbot-devel --hostname munbot-devel -u munbot \
-	-p 127.0.0.1:6060:6060 -v ${PWD}:/go/src/munbot jrmsdev/munbot:devel
+	-p 127.0.0.1:6060:6060 \
+	-p 127.0.0.1:3000:3000 \
+	-v ${PWD}:/go/src/munbot jrmsdev/munbot:devel
 
 exit 0
