0d5a1bd45e900253415d09a3f6fbc64b47be6f30
Jeremías <jrmsdev@gmail.com>
2020-07-01 04:07:38 -0300

    docker devel CA certificates

--- docker/devel/ca/gen.sh
+++ docker/devel/ca/gen.sh
@@ -2,4 +2,13 @@
 set -eu
 ca=/go/src/munbot/docker/devel/ca/run.sh
 ${ca} -newca
+
+cd /go/src/munbot/docker/devel/ca/db
+mkdir -vp new certs private
+cd new
+
+${ca} -newcert
+mv -v ./newcert.pem ../certs/munbot.devel.pem
+mv -v ./newkey.pem ../private/munbot.devel.pem
+
 exit 0
