d3bbe9b5e296f2fb7b03d8fdfa6f30c3e80d46b3
Jeremías <jrmsdev@gmail.com>
2020-07-01 06:42:53 -0300

    docker mount devel ca ssl files

--- docker/devel/run.sh
+++ docker/devel/run.sh
@@ -4,6 +4,7 @@ set -eu
 docker run -it --rm --name munbot-devel --hostname munbot-devel -u munbot \
 	-p 127.0.0.1:6060:6060 \
 	-p 127.0.0.1:3000:3000 \
+	-v ${PWD}/docker/devel/ca/db:/etc/ssl/munbot \
 	-v ${PWD}/vendor:/go/src/vendor_deps \
 	-v ${PWD}:/go/src/munbot jrmsdev/munbot:devel
 
