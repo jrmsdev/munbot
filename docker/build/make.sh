102dd0fc8bbdb36cd1df7267789d6b2a70699431
Jeremías <jrmsdev@gmail.com>
2020-07-05 02:09:11 -0300

    docker build cross platform

--- docker/build/make.sh
+++ docker/build/make.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 set -eu
+GOOS=${GOOS:-''}
+GOARCH=${GOARCH:-''}
 exec docker run -it --rm --network none --name munbot-build --hostname munbot-build \
-	-u munbot -v ${PWD}:/go/src/munbot jrmsdev/munbot:build $@
+	-u munbot -e "GOOS=${GOOS}" -e "GOARCH=${GOARCH}" \
+	-v ${PWD}:/go/src/munbot jrmsdev/munbot:build $@
