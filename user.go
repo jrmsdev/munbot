af8291fdf64e6d0b178cad82456d8ee7dd439074
Jeremías <jrmsdev@gmail.com>
2020-07-05 18:21:00 -0300

    remove old config code

--- user.go
+++ user.go
@@ -4,15 +4,15 @@
 package munbot
 
 import (
-	"github.com/jrmsdev/munbot/config"
+	"github.com/jrmsdev/munbot/config2"
 	"github.com/jrmsdev/munbot/log"
 )
 
 type User struct {
-	*config.User
+	*config2.User
 }
 
-func NewUser(cfg *config.User) *User {
+func NewUser(cfg *config2.User) *User {
 	log.Debugf("new '%s'", cfg.Name)
 	return &User{cfg}
 }
