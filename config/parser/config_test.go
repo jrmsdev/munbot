f3aa09489b6fc15b0b9754f21aff9a924779bf3a
Jeremías <jrmsdev@gmail.com>
2020-07-17 20:55:31 -0300

    config test struct

--- config/parser/config_test.go
+++ config/parser/config_test.go
@@ -5,6 +5,9 @@ package parser
 
 import (
 	"testing"
+
+	"github.com/munbot/master/testing/assert"
+	"github.com/munbot/master/testing/require"
 )
 
 var tdef DB = DB{
@@ -15,34 +18,41 @@ var tdef DB = DB{
 
 var tcfg = []byte(`{"master":{"name":"testing"}}`)
 
+type testCfg struct {
+	t *testing.T
+	test *Config
+	assert *assert.Assertions
+	require *require.Assertions
+}
+
+func newTestCfg(t *testing.T) *testCfg {
+	return &testCfg{t, New(), assert.New(t), require.New(t)}
+}
+
+func (c *testCfg) setDefaults() {
+	c.test.SetDefaults(tdef)
+}
+
 func TestNew(t *testing.T) {
-	c := New()
-	t.Logf("%#v", c)
-	c.SetDefaults(tdef)
-	t.Logf("%#v", c)
+	c := newTestCfg(t)
+	c.setDefaults()
 
-	blob, err := c.Dump()
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("%s", blob)
+	blob, err := c.test.Dump()
+	c.require.NoError(err, "dump error")
+	c.assert.Equal(blob, []byte(`{"master":{"name":"munbot"}}`), "dump blob")
 
-	s := c.Section("master")
-	t.Log(s)
-	t.Logf("master.name: %s", s.Get("name"))
-	t.Logf("missing bool: %v", s.GetBool("missing"))
+	s := c.test.Section("master")
+	c.require.Equal(s.Name(), "master", "section name")
+	c.assert.Equal(s.Get("name"), "munbot", "master.name value")
+	c.assert.Equal(s.Get("missing"), "ECFGMISS:master.missing", "get missing value")
 
-	s = c.Section("master.api")
-	t.Log(s)
-	t.Logf("master.api.enable: %v", s.GetBool("enable"))
+	s = c.test.Section("missing")
+	c.require.Equal(s.Name(), "ECFGSECT:missing", "missing section name")
 
-	err = c.Load(tcfg)
-	if err != nil {
-		t.Fatal(err)
-	}
-	blob, err = c.Dump()
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("%s", blob)
+	err = c.test.Load(tcfg)
+	c.require.NoError(err, "load error")
+
+	blob, err = c.test.Dump()
+	c.require.NoError(err, "dump error")
+	c.assert.Equal(blob, tcfg, "dump blob")
 }
