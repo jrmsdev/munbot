1522f76b10e0d7090b4d3a7b43fade3e6c148d2d
Jeremías <jrmsdev@gmail.com>
2020-07-26 03:54:47 -0300

    config parse log flags

--- config/flags.go
+++ config/flags.go
@@ -5,21 +5,36 @@ package config
 
 import (
 	"flag"
+
+	"github.com/munbot/master/log"
 )
 
 // Flags holds some configuration settings that can be overriden from cmd args.
 type Flags struct {
 	Debug   bool
+	Quiet   bool
+	Verbose bool
 	Profile string
 }
 
 // Set sets the flags to the provided handler.
 func (f *Flags) Set(fs *flag.FlagSet) {
 	fs.BoolVar(&f.Debug, "debug", false, "enable debug settings")
+	fs.BoolVar(&f.Quiet, "q", false, "set quiet mode")
+	fs.BoolVar(&f.Verbose, "v", false, "set verbose mode")
 	fs.StringVar(&f.Profile, "profile", "default", "profile `name`")
 }
 
 // Parse parses the flags.
 func (f *Flags) Parse() error {
+	if f.Debug {
+		log.DebugEnable()
+	}
+	if f.Quiet {
+		log.SetQuiet()
+	}
+	if f.Verbose {
+		log.SetVerbose()
+	}
 	return nil
 }
