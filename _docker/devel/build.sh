f73b7df82f23a6d42ad3a5cc5967cf8786fcbd37
Jeremías <jrmsdev@gmail.com>
2020-08-01 20:12:51 -0300

    docker devel use vendor deps on build

--- _docker/devel/build.sh
+++ _docker/devel/build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -eu
+rm -rvf ./_docker/devel/tmp
 mkdir -vp ./_docker/devel/tmp
-cp -va ./go.mod ./go.sum ./_docker/devel/tmp
+cp -va ./go.mod ./go.sum ./_docker/devel/tmp/
+install -v -C -d ./vendor ./_docker/devel/tmp/vendor
 docker build --rm -t munbot/master:devel ./_docker/devel
+rm -vrf ./_docker/devel/tmp
 exit 0
