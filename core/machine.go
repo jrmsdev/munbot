0dba64268ce8680d3b8f7b1d4d0c096ac9c36f20
Jeremías <jrmsdev@gmail.com>
2020-08-02 02:05:31 -0300

    core state machine remove unused methods from interface

--- core/machine.go
+++ core/machine.go
@@ -4,16 +4,12 @@
 package core
 
 import (
-	"context"
-
 	"github.com/munbot/master/config"
 	"github.com/munbot/master/core/flags"
 )
 
 type Machine interface {
 	SetState(StateID) error
-	Context() context.Context
-	WithContext(context.Context) (context.Context, error)
 	Config() *config.Config
 	ConfigFlags() *config.Flags
 	CoreFlags() *flags.Flags
