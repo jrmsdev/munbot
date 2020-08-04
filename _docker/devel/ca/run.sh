f4b2feea489011bc25465126255f58377fb762bf
Jeremías <jrmsdev@gmail.com>
2020-08-04 17:48:50 -0300

    remove old tinyca files

--- _docker/devel/ca/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -eu
-export OPENSSL_CATOP=/var/tmp/munbot/ca
-export OPENSSL_CONFIG=./openssl.cnf
-exec /etc/ssl/misc/CA.pl $@
--- _docker/devel/run.sh
+++ _docker/devel/run.sh
@@ -3,6 +3,5 @@ set -eu
 exec docker run -it --rm --name munbot-devel --hostname devel.munbot.local \
 	-p 127.0.0.1:6060:6060 \
 	-p 127.0.0.1:6492:6492 \
-	-v ${PWD}/_docker/devel/ca/files:/home/munbot/.config/munbot/default/ssl \
 	-v ${PWD}:/munbot/src/master \
 	-u munbot munbot/master:devel $@
