19f12bef938ad45549bac5305cfbdfd1522e6837
Jeremías <jrmsdev@gmail.com>
2020-08-04 21:28:48 -0300

    test testing/suite package

--- testing/suite/suite.go
+++ testing/suite/suite.go
@@ -1,8 +1,7 @@
 // Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 // See LICENSE file.
 
-// Package suite is just a handy shorcut (?) to import
-// github.com/stretchr/testify/suite functionality.
+// Package suite wraps github.com/stretchr/testify/suite package.
 package suite
 
 import (
@@ -11,14 +10,17 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
+// Suite wraps suite.Suite.
 type Suite struct {
 	*suite.Suite
 }
 
+// New creates a new suite.Suite wrapper.
 func New() *Suite {
 	return &Suite{new(suite.Suite)}
 }
 
+// Run runs the suite.
 func Run(t *testing.T, s suite.TestingSuite) {
 	suite.Run(t, s)
 }
