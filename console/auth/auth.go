9f98efbcb11d67b20c057a752f414975520ba607
Jeremías <jrmsdev@gmail.com>
2020-08-08 00:20:46 -0300

    console auth check/create ca dir

--- console/auth/auth.go
+++ console/auth/auth.go
@@ -5,14 +5,20 @@
 package auth
 
 import (
+	"errors"
+	"os"
 	"path/filepath"
 
 	"github.com/munbot/master/log"
 )
 
+var ErrCADir error = errors.New("auth: invalid CA dir")
+
 // Auth implemenst the console auth manager.
 type Auth struct {
-	dir string
+	dir  string
+	priv string
+	pub  string
 }
 
 // New creates a new Auth instance.
@@ -22,7 +28,22 @@ func New() *Auth {
 
 // Configure sets up the CA directory.
 func (a *Auth) Configure(cadir string) error {
-	a.dir = filepath.Clean(cadir)
+	var err error
+	a.dir, err = filepath.Abs(cadir)
+	if a.dir == "." {
+		return ErrCADir
+	}
+	if err != nil {
+		return err
+	}
+	if err = os.MkdirAll(a.dir, 0700); err != nil {
+		return err
+	}
 	log.Debugf("CA dir: %s", a.dir)
+	return a.setup()
+}
+
+func (a *Auth) setup() error {
+	log.Debug("setup")
 	return nil
 }
