8f16289194314614f8f0560247233a48e6a75cc5
Jeremías <jrmsdev@gmail.com>
2020-08-11 20:51:12 -0300

    env test envs config dir

--- env/env_test.go
+++ env/env_test.go
@@ -4,6 +4,7 @@
 package env_test
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/munbot/master/env"
@@ -12,8 +13,10 @@ import (
 
 func TestEnvFile(t *testing.T) {
 	check := assert.New(t)
-	check.Equal("__UNSET__", env.Get("MBTEST_UNSET_ENV_VAR"))
 	check.Equal("test", env.Get("MBENV"), "MBENV")
+	cfgdir, err := filepath.Abs(filepath.FromSlash("."))
+	check.NoError(err)
+	check.Equal(cfgdir, env.Get("MBENV_CONFIG"), "MBENV_CONFIG")
 	check.Equal("test.env", env.Get("MBTEST_ENVFILE"), "MBTEST_ENVFILE")
 	check.Equal("testing", env.Get("MB_PROFILE"), "MB_PROFILE")
 	check.Equal("etc", env.Get("MB_CONFIG"), "MB_CONFIG")
