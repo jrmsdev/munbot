2ff186587beae639dabf04d7bf675d1124080301
Jeremías <jrmsdev@gmail.com>
2020-08-16 00:33:02 -0300

    config flags add more tests

--- config/flags_test.go
+++ config/flags_test.go
@@ -14,12 +14,18 @@ func newTestFS() *flag.FlagSet {
 func (s *Suite) TestFlagsDefaults() {
 	f := NewFlags(newTestFS())
 	s.False(f.Debug, "default debug")
+	s.False(f.Quiet, "default quiet")
+	s.False(f.Verbose, "default verbose")
+	s.Equal("", f.Name, "default name")
 	s.Equal("", f.Profile.Name, "default profile")
 }
 
 func (s *Suite) TestFlagsParse() {
 	f := NewFlags(newTestFS())
 	f.Parse()
-	s.False(f.Debug, "default debug")
-	s.Equal("testing", f.Profile.Name, "default profile")
+	s.False(f.Debug)
+	s.False(f.Quiet)
+	s.False(f.Verbose)
+	s.Equal("master", f.Name)
+	s.Equal("testing", f.Profile.Name)
 }
