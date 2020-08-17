9f35b16cad1b0f7e5fefb59ebb8fafb507d50105
Jeremías <jrmsdev@gmail.com>
2020-08-17 05:22:28 -0300

    docker devel expose api server

--- _docker/devel/run.sh
+++ _docker/devel/run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -eu
 exec docker run -it --rm --name munbot-devel --hostname devel.munbot.local \
+	-e 'MBAPI_ADDR=devel.munbot.local' \
+	-e 'MBCONSOLE_ADDR=devel.munbot.local' \
 	-p 127.0.0.1:6060:6060 \
 	-p 127.0.0.1:6490:6490 \
 	-p 127.0.0.1:6492:6492 \
