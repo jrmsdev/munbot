a1639b54e18c84ab80dc9aa2c555934599d24e90
Jeremías <jrmsdev@gmail.com>
2020-08-18 19:55:39 -0300

    v0 master flags

---
+++ v0/flags_test.go
@@ -0,0 +1,34 @@
+// Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
+// See LICENSE file.
+
+package master
+
+import (
+	"flag"
+	"testing"
+
+	"github.com/munbot/master/v0/testing/assert"
+)
+
+func newTestFS() *flag.FlagSet {
+	return flag.NewFlagSet("testing", flag.PanicOnError)
+}
+
+func TestFlagsSet(t *testing.T) {
+	assert := assert.New(t)
+	f := NewFlags()
+	f.Set(newTestFS())
+	assert.Equal("", f.apiAddr, "default api.addr")
+	assert.Equal(uint(0), f.apiPort, "default api.port")
+}
+
+func TestFlagsDefaults(t *testing.T) {
+	assert := assert.New(t)
+	f := NewFlags()
+	f.Set(newTestFS())
+	f.Parse()
+	assert.Equal("", f.apiAddr, "default api.addr")
+	assert.Equal(uint(0), f.apiPort, "default api.port")
+	assert.Equal("", f.consoleAddr, "default console.addr")
+	assert.Equal(uint(0), f.consolePort, "default console.port")
+}
