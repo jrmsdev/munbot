a292f38007eea95ceaefc8e844b0bcc881b4a424
Jeremías <jrmsdev@gmail.com>
2020-08-19 00:28:34 -0300

    gofmt src list

--- v0/gofmt.sh
+++ v0/gofmt.sh
@@ -2,11 +2,14 @@
 if test "X${1}" = 'Xall'; then
 	exec gofmt -w -l -s .
 fi
-exec gofmt -w -l -s \
-	cmd \
-	config \
-	env \
-	log \
-	testing \
-	version \
-	vfs
+srclist() {
+	for src in $(ls ./*.go); do
+		echo ${src}
+	done
+	for srcd in $(ls -d ./* | grep -v vendor | grep -vE '^\./_'); do
+		if test -d ${srcd}; then
+			echo ${srcd}
+		fi
+	done
+}
+exec gofmt -w -l -s $(srclist)
