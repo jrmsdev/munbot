13de6f118cbd73c9ffcb7ae1b1dd02559ac4bd19
Jeremías <jrmsdev@gmail.com>
2020-08-24 20:34:04 -0300

    hash strings util

---
+++ v0/utils/hash/hash.go
@@ -0,0 +1,14 @@
+// Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
+// See LICENSE file.
+
+// Package hash implements some hashing utils.
+package hash
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+func Sum(s string) string {
+	return fmt.Sprintf("%s", sha256.Sum256([]byte(s)))
+}
