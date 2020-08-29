a17a5c1e3dd6a700c47f881bf9ed186468acb296
Jeremías <jrmsdev@gmail.com>
2020-08-28 22:39:12 -0300

    session add/remove

--- v0/internal/session/session.go
+++ v0/internal/session/session.go
@@ -5,6 +5,8 @@
 package session
 
 import (
+	"sync"
+
 	"github.com/munbot/master/v0/internal/user"
 	"github.com/munbot/master/v0/log"
 	"github.com/munbot/master/v0/utils/uuid"
@@ -28,12 +30,57 @@ func (t Token) String() string {
 	return uuid.ToString(uuid.UUID(t))
 }
 
+type Session struct {
+	id  Token
+	uid user.ID
+	fp  string
+}
+
+type sm struct {
+	*sync.Mutex
+	sess map[Token]*Session
+}
+
+var sess *sm
+
+func init() {
+	sess = &sm{new(sync.Mutex), make(map[Token]*Session)}
+}
+
+func (m *sm) Add(sid Token, uid user.ID, fp string) (ok bool) {
+	m.Lock()
+	defer m.Unlock()
+	if _, found := m.sess[sid]; found {
+		return false
+	}
+	m.sess[sid] = &Session{sid, uid, fp}
+	return true
+}
+
+func (m *sm) Remove(sid Token) (ok bool) {
+	m.Lock()
+	defer m.Unlock()
+	if _, found := m.sess[sid]; found {
+		return false
+	}
+	delete(m.sess, sid)
+	return true
+}
+
 func Login(sid Token, uid user.ID, fp string) error {
+	log.Debugf("%s login", sid)
+	if !sess.Add(sid, uid, fp) {
+		return log.Errorf("Session found %s.", sid)
+	}
 	log.Printf("User login %s %s.", uid, sid)
 	return nil
 }
 
 func Logout(sid Token) error {
+	log.Debugf("%s logout", sid)
+	if !sess.Remove(sid) {
+		return log.Errorf("Session not found %s.", sid)
+	}
 	log.Printf("User logout %s.", sid)
 	return nil
 }
