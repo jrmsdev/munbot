6a4824ba031835d0dfa92a10484215e5d63be883
Jeremías <jrmsdev@gmail.com>
2020-09-02 19:29:47 -0300

    vendor script --upgrade

--- v0/vendor.sh
+++ v0/vendor.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 set -eu
+if test '--upgrade' = "${1:-''}"; then
+	mkdir -p ./_build
+	cp -a go.mod ./_build/go.mod
+	cat ./_build/go.mod | sed 's#^\t\([^ ]*\) .*#   \1 latest#' >go.mod
+fi
 go mod vendor
 exec go mod tidy
