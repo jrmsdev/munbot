c51ce6797b22feac992a8314f8a7e5815fdd1788
Jeremías <jrmsdev@gmail.com>
2020-09-02 19:35:02 -0300

    vendor upgrade ignore indirect deps

--- v0/vendor.sh
+++ v0/vendor.sh
@@ -3,7 +3,7 @@ set -eu
 if test '--upgrade' = "${1:-''}"; then
 	mkdir -p ./_build
 	cp -a go.mod ./_build/go.mod
-	cat ./_build/go.mod | sed 's#^\t\([^ ]*\) .*#   \1 latest#' >go.mod
+	cat ./_build/go.mod | sed 's#^\t\([^ ]*\) v[^ ]*$#	\1 latest#' >go.mod
 fi
 go mod vendor
 exec go mod tidy
